bba05f14b273707152e20d228be702c3a83ad514
codinTarantino <164989745+codinTarantino@users.noreply.github.com>
2025-11-02 19:35:55 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,10 +1,11 @@
 @echo off
 echo ===== Compiling Java Files =====
-if not exist bin mkdir bin
 cd %WORKSPACE%
-javac -d bin src\com\example\App.java src\com\example\AppTest.java
+if not exist bin mkdir bin
+javac -d bin .\\simple-java-devops\\src\\com\\example\\App.java .\\simple-java-devops\\src\\com\\example\\AppTest.java
 if %ERRORLEVEL% NEQ 0 (
     echo ❌ Build failed!
     exit /b 1
 )
 echo ✅ Compilation successful!
+
